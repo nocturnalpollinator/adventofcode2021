fb2cdc1ae3d33f33dc53eab1ba95a05342ef2f86
Robin <hejrobinjonsson@gmail.com>
2021-12-01 11:35:25 +0100

    oneliner

--- 01/main.js
+++ 01/main.js
@@ -1,10 +1,5 @@
 var n = require('fs').readFileSync('./input.txt', 'utf-8').split('\n').map(a => parseInt(a))
 
-var count = n.reduce((p, c, i) => [
-    // Counting increments of each step
-    p[0] + (c > n[i-1]), 
-    // Counting the windows increments
-    i < 3 ? 0 : p[1] + (n[i] > n[i-3])
-], [0, 0]);
+var count = n.reduce((p, c, i) => [p[0] + (c > n[i-1]), i < 3 ? 0 : p[1] + (n[i] > n[i-3])], [0, 0]);
 
 console.log('Part I: ' + count[0], '\nPart II: ' + count[1])
