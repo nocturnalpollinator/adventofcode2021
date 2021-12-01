58046d4886d3f390ab0cd6f9744ac0ca25f390df
Robin <hejrobinjonsson@gmail.com>
2021-12-01 11:10:08 +0100

    Day 1

--- 01/main.js
+++ 01/main.js
@@ -1,7 +1,7 @@
-var n = require('fs').readFileSync('./input.txt', 'utf-8').split('\n').map(e => parseInt(e))
+var n = require('fs').readFileSync('./input.txt', 'utf-8').split('\n').map(a => parseInt(a))
 
-var count = n.reduce((p, e, i) => [
-    p[0] + (e > n[i-1]),
+var count = n.reduce((p, c, i) => [
+    p[0] + (c > n[i-1]),
     i < 3 ? 0 : p[1] + (n.slice(i-2, i+1).reduce((a, b) => a + b) > n.slice(i-3, i).reduce((a, b) => a + b))
 ], [0, 0]);
 
