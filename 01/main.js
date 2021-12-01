1cdba73aefeb9920fe4ac3f7f633bef09f340430
Robin <hejrobinjonsson@gmail.com>
2021-12-01 10:18:55 +0100

    Changed map to reduce

--- 01/main.js
+++ 01/main.js
@@ -1,10 +1,8 @@
 var n = require('fs').readFileSync('./input.txt', 'utf-8').split('\n').map(e => parseInt(e))
-var incr = incrW = 0
 
-n.map((e, i) => {
-    incr += e > n[i-1]
-    if (i < 3) return
-    incrW += n.slice(i-2, i+1).reduce((a, b) => a + b) > n.slice(i-3, i).reduce((a, b) => a + b)
-})
+var count = n.reduce((p, e, i) => [
+    p[0] + (e > n[i-1]),
+    i < 3 ? 0 : p[1] + (n.slice(i-2, i+1).reduce((a, b) => a + b) > n.slice(i-3, i).reduce((a, b) => a + b))
+], [0, 0]);
 
-console.log('Part I: ' + incr, '\nPart II: ' + incrW)
+console.log('Part I: ' + count[0], '\nPart II: ' + count[1])
