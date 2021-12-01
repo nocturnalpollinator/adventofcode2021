80457c7ad5266896a2d8401c661913ec90b9edea
Robin <hejrobinjonsson@gmail.com>
2021-12-01 11:44:30 +0100

    I need to stop now

--- 01/main.js
+++ 01/main.js
@@ -1,5 +1,5 @@
 var n = require('fs').readFileSync('./input.txt', 'utf-8').split('\n').map(a => parseInt(a))
 
-var count = n.reduce((p, c, i) => [p[0] + (c > n[i-1]), i < 3 ? 0 : p[1] + (n[i] > n[i-3])], [0, 0]);
+var count = n.reduce((p, c, i) => [p[0] + (c > n[i-1]), i < 3 ? 0 : p[1] + (n[i] > n[i-3])], [0, 0])
 
-console.log('Part I: ' + count[0], '\nPart II: ' + count[1])
+console.log(`Part I: ${count[0]}\nPart II: ${count[1]}`)
