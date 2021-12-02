6764df9baab9196409e6bff5885f1598d9c1899a
Robin <hejrobinjonsson@gmail.com>
2021-12-02 08:29:57 +0100

    Day 2 - just making it stupid

--- 02/main.js
+++ 02/main.js
@@ -1,9 +1,9 @@
 var count = require('fs').readFileSync('./input.txt', 'utf-8').split('\n').reduce((p, c) => {
-    var x = c.split(' ').map((s, i) => i > 0 ? parseInt(s) : s)
+    var x = c.split(' ').map((s, i) => i > 0 ? parseInt(s) : s[0])
     return [
-        x[0] == 'forward' ? p[0] += x[1] : p[0], // Horizontal
-        x[0] == 'down'    ? p[1] += x[1] : x[0] == 'up' ? p[1] -= x[1] : p[1], // Depth
-        x[0] == 'forward' ? p[2] += x[1] * p[1] : p[2] // Aim
+        x[0] == 'f' ? p[0] += x[1] : p[0], // Horizontal
+        x[0] == 'd' ? p[1] += x[1] : x[0] == 'u' ? p[1] -= x[1] : p[1], // Depth
+        x[0] == 'f' ? p[2] += x[1] * p[1] : p[2] // Aim
     ]
 }, [0, 0, 0])
 
