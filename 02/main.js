131a7a91f1943af2d54067cfd9674790bfd123f6
Robin <hejrobinjonsson@gmail.com>
2021-12-02 06:56:42 +0100

    Day 2

--- 02/main.js
+++ 02/main.js
@@ -2,9 +2,9 @@ var count = require('fs').readFileSync('./input.txt', 'utf-8').split('\n').reduc
     var dir = c.split(' ')[0]
     var len = parseInt(c.split(' ')[1])
     return [
-        dir == 'forward' ? p[0] += len : p[0],
-        dir == 'down'    ? p[1] += len : dir == 'up' ? p[1] -= len : p[1],
-        dir == 'forward' ? p[2] += len * p[1] : p[2]
+        dir == 'forward' ? p[0] += len : p[0], // Horizontal
+        dir == 'down'    ? p[1] += len : dir == 'up' ? p[1] -= len : p[1], // Depth
+        dir == 'forward' ? p[2] += len * p[1] : p[2] // Aim
     ]
 }, [0, 0, 0])
 
